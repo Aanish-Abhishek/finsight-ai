5ca802cb2cf540f2fbaa809717efcdbf71be8864
Aanish-Abhishek <aanishabhishek66@gmail.com>
2026-05-24 22:56:09 +0530

    remove unused temp files

--- temp_prompt_api.py
+++
@@ -1,4 +0,0 @@
-from langchain_core.prompts import PromptTemplate
-print('has_format', hasattr(PromptTemplate, 'format'))
-print('has_format_prompt', hasattr(PromptTemplate, 'format_prompt'))
-print('methods', [n for n in dir(PromptTemplate) if 'format' in n.lower()])
